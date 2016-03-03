26fe4aa2848e95fcd0caf48a153a0239d550053a
Jake Johnson <jake@fastdivision.com>
2016-03-03 12:56:34 -0800

    Add new /v2/validation and /v2/summary_rates endpoints with specs

---
+++ test/specs/ValidationTest.php
@@ -0,0 +1,25 @@
+<?php
+if (!class_exists('TaxJarTest')) {
+  require __DIR__ . '/../TaxJarTest.php';
+}
+
+class ValidationTest extends TaxJarTest {
+  public function test_validation() {
+    $this->http->mock
+        ->when()
+            ->methodIs('GET')
+            ->pathIs('/validation?vat=FR40303265045')
+        ->then()
+            ->statusCode(200)
+            ->body(file_get_contents(__DIR__ . "/../fixtures/validation.json"))
+        ->end();
+
+    $this->http->setUp();
+    
+    $response = $this->client->validate([
+      'vat' => 'FR40303265045'
+    ]);
+    
+    $this->assertJsonStringEqualsJsonFile(__DIR__ . '/../fixtures/validation.json', json_encode(["validation" => $response]));
+  }
+}
