edf3adfc89a6937ce53e45d38828bdb7434d09b9
Jake Johnson <jake@fastdivision.com>
2018-10-24 15:07:37 -0700

    Add spec for address validation

--- test/specs/ValidationTest.php
+++ test/specs/ValidationTest.php
@@ -5,6 +5,32 @@ if (!class_exists('TaxJarTest')) {
 
 class ValidationTest extends TaxJarTest
 {
+    public function testValidateAddress()
+    {
+        $this->http->mock
+            ->when()
+            ->methodIs('POST')
+            ->pathIs('/addresses/validate')
+            ->then()
+            ->statusCode(200)
+            ->body(file_get_contents(__DIR__ . "/../fixtures/addresses.json"))
+            ->end();
+
+        $this->http->setUp();
+
+        $response = $this->client->validateAddress([
+            'country' => 'US',
+            'state' => 'AZ',
+            'zip' => '85297',
+            'city' => 'Gilbert',
+            'street' => '3301 South Greenfield Rd'
+        ]);
+
+        $this->assertJsonStringEqualsJsonFile(__DIR__ . '/../fixtures/addresses.json', json_encode([
+            "addresses" => $response
+        ]));
+    }
+
     public function testValidation()
     {
         $this->http->mock
