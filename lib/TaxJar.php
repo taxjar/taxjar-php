d11dc343cf2f5a9f7fdbe1db049f790fdcc436b2
ScottRudiger <scottrudiger@gmail.com>
2020-06-03 11:24:44 -0700

    Add a default error message when error or detail properties don't exist

--- lib/TaxJar.php
+++ lib/TaxJar.php
@@ -36,19 +36,15 @@ class TaxJar
             if ($response->getStatusCode() >= 400) {
                 $data = json_decode($response->getBody());
 
-                if ($data && $data->error && $data->detail) {
-                    throw new Exception(
-                        sprintf(
-                            '%s %s – %s',
-                            $response->getStatusCode(),
-                            $data->error,
-                            $data->detail
-                        ),
-                        $response->getStatusCode()
-                    );
-                }
-
-                throw new Exception($response);
+                throw new Exception(
+                    sprintf(
+                        '%s %s – %s',
+                        $response->getStatusCode(),
+                        isset($data->error) ? $data->error : 'something unexpected occurred',
+                        isset($data->detail) ? $data->detail: 'please try again'
+                    ),
+                    $response->getStatusCode()
+                );
             }
 
             return $response;
