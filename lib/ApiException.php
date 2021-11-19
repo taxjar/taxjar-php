994d07c91fd6549bea2087d96b223e06f4890ec5
Tim Martin <git@timmartin.me>
2021-11-19 12:48:54 -0500

    Add more type hints to ApiException

--- lib/ApiException.php
+++ lib/ApiException.php
@@ -3,11 +3,14 @@ namespace TaxJar;
 
 class ApiException extends Exception
 {
+    /**
+     * @var \Psr\Http\Message\ResponseInterface
+     */
     protected $response;
 
     /**
      * @param string $message
-     * @param \Psr\Http\Message\ResponseInterface$response
+     * @param \Psr\Http\Message\ResponseInterface $response
      * @param \Throwable|null $previous
      */
     public function __construct($message, $response, $previous = null)
@@ -16,6 +19,9 @@ class ApiException extends Exception
         $this->response = $response;
     }
 
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function getResponse()
     {
         return $this->response;
